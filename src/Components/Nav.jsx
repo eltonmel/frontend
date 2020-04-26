d0a54f8c450fc577ff5c869d03b8ef5e540f5e92
Elton Mel <eltondemel2010@hotmail.com>
2020-04-26 15:44:09 -0300

    Sidebar menu stylized

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,4 +2,48 @@ import React from 'react';
 
 import '@/Styles/Nav.scss';
 
-export default () => <nav className="nav">Navigation</nav>;
+export default () => (
+  <nav className="nav">
+    <ul>
+      <li>
+        <a href="#">
+          <i class="fas fa-bars"></i>Menu 1
+        </a>
+        <ul className="nav-flyout">
+          <li>
+            <a href="#">
+              <i class="fas fa-bars"></i>SubMenu 1
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fas fa-bars"></i>SubMenu 2
+            </a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fas fa-bars"></i>Menu 2
+        </a>
+        <ul className="nav-flyout">
+          <li>
+            <a href="#">
+              <i class="fas fa-bars"></i>SubMenu 1
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fas fa-bars"></i>SubMenu 2
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fas fa-bars"></i>SubMenu 3
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
+);
